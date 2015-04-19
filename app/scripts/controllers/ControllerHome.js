7c11f8449c941b816923900c00b9707b2fb8cdad
Derrick Roccka <derrickroccka@gmail.com>
2015-04-19 17:07:19 +0200

    Effects and animations added to the home logo

--- app/scripts/controllers/ControllerHome.js
+++ app/scripts/controllers/ControllerHome.js
@@ -8,14 +8,36 @@
 	* @description This is the controller of the home
 	* @param {} 
 	**/
-	function ControllerHome(){
+	function ControllerHome($timeout){
 		var vm = this;
+
+		vm.animations = {
+			logo: {
+				activeLayer: 0,
+				layerLimit: 3,
+				init: function(){
+					$timeout(function(){
+						vm.animations.logo.activeLayer++;
+					},500);
+					$timeout(function(){
+						vm.animations.logo.activeLayer++;
+					},1000);
+					$timeout(function(){
+						vm.animations.logo.activeLayer++;
+					},1500);
+				}
+			}
+		};
 		vm.rotatingWords = ['a dev','...','this(8)','dy','a youtuber','a gamer','#fifalover','a man','creativity','creactivity','professional','happiness','a robot','a creator','a crazy guy','ingenio'];
+
+		vm.animations.logo.init();
+
+
 	}
 	//Injecting dependencies into ControllerHome function
-	ControllerHome.$inject = [];
+	ControllerHome.$inject = ['$timeout'];
 	//Attatching ControllerHome function into the app
 	angular
-		.module('drkv2App')
-		.controller('ControllerHome',ControllerHome);
+	.module('drkv2App')
+	.controller('ControllerHome',ControllerHome);
 })();
