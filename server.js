724f9eb597254028b1f2b8f260c9f17c76cf33cb
Derrick Roccka <derrickroccka@gmail.com>
2015-04-29 20:32:51 +0200

    server.js (basic) and Procfile added

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var gzippo = require('gzippo');
+var express = require('express');
+var app = express();
+
+app.use(express.logger('dev'));
+app.use(gzippo.staticGzip('' + __dirname + '/dist'));
+app.listen(process.env.PORT || 5000);
+
+app.get('*', function(req, res){
+	res.redirect('/');
+});
